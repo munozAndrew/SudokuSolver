efb6691aa490bdf32e65cfcc35bdeda80f6d4b58
munozAndrew <115670550+munozAndrew@users.noreply.github.com>
2023-04-22 14:32:14 -0700

    Main Created Sudoku board and print board function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# 2d sudoku board that has 0s represent an empty cell
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -10,10 +11,21 @@ board = [
     [0,4,9,2,0,6,0,0,7],
 ]
 
-def print_board(brd):
 
+def print_board(brd):
+    #loops through rows
     for i in range(len(brd)):
-        print(i)
+        #print horizontal line after every 3 rows 
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - -")
+        #loops through columns
         for j in range(len(brd[0])):
-            print(j)
-print_board(board)
+            #print vertical lines after every 3 columns 
+            if j % 3 == 0 and j != 0:
+                print(" | " , end="")
+            #print value of the cell
+            if j == 8:
+                print(brd[i][j])
+            else:
+                print(str(brd[i][j]) + " ", end="")
+
