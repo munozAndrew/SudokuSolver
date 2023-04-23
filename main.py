e27f10d2eba24f2c44ca5293227e61be49092884
munozAndrew <115670550+munozAndrew@users.noreply.github.com>
2023-04-22 17:46:23 -0700

    Completed Added functions to find an empty cell, test validity of a number, and solve the puzzle using backtracking.

--- main.py
+++ main.py
@@ -11,6 +11,66 @@ board = [
     [0,4,9,2,0,6,0,0,7],
 ]
 
+test2 = [
+    [3,0,5,4,0,2,0,6,0],
+    [4,9,0,7,6,0,1,0,8],
+    [6,0,0,1,0,3,2,4,5],
+    [0,0,3,9,0,0,5,8,0],
+    [9,6,0,0,5,8,7,0,3],
+    [0,8,1,3,0,4,0,9,2],
+    [0,5,0,6,0,1,4,0,0],
+    [2,0,0,5,4,9,0,7,0],
+    [1,4,9,0,0,7,3,0,6],
+]
+
+def solver(brd):
+    #base case: exits loop if no more empty cells exist
+    find_empty = find_empty_cell(brd)
+    if not find_empty:
+        return True
+    #returns cords for an empty cell
+    else:
+        row, col = find_empty
+    #loops through potential numbers
+    for i in range(1,10):
+        #inputs value into empty cell
+        if valid(brd, i, (row, col)):
+            brd[row][col] = i
+            
+            #recursively call the solver function to fill the next empty cell
+            if solver(brd):
+                #runs if board is solved
+                return True
+            #reset cell to 0 if the current value didnt lead to a working solution
+            brd[row][col] = 0
+    #backtraces and tries a different value if no valid values were found for the current cell
+    return False
+            
+            
+
+
+
+def valid(brd, num, cord):
+    #check row
+    for i in range(len(brd[0])):
+        if brd[cord[0]][i] == num and cord[1] != i:
+            return False
+    #check columm
+    for i in range(len(brd)):
+        if brd[i][cord[1]] == num and cord[0] != i:
+            return False
+    
+    y_box = cord[0] // 3
+    x_box = cord[1] // 3
+    #checks 3x3 square by creating a new matrix essentially
+    for i in range(y_box * 3, y_box * 3 + 3):
+        for j in range(x_box * 3, x_box * 3 + 3):
+            if brd[i][j] == num and (i,j) != cord:
+                return False 
+    return True
+                
+
+            
 
 def print_board(brd):
     #loops through rows
@@ -29,3 +89,20 @@ def print_board(brd):
             else:
                 print(str(brd[i][j]) + " ", end="")
 
+def find_empty_cell(brd):
+    #loops through rows
+    for i in range(len(brd)):
+        #loops thorugh columns
+        for j in range(len(brd[0])):
+            #returns row and column for empty cell
+            if brd[i][j] == 0:
+                #returns (row, column)
+                return (i, j) 
+    #returns none if all cells are fill AKA finished the puzzle
+    return None
+
+
+print_board(test2)
+print("======================")
+solver(test2)
+print_board(test2)
